cb89bff4e51816d701f025dead4a9a4676ef14b0
Heim <heimzeng@gmail.com>
2019-03-07 15:28:39 +0800

    protobuf module

---
+++ python/protobuf/main.py
@@ -0,0 +1,20 @@
+from dst import room_pb2
+room = room_pb2.Room()
+room.id = 11
+
+p1 = room.persons.add()
+p1.id = 1
+p1.name = 'heim'
+p2 = room.persons.add()
+p2.id = 3
+p2.name = 'h'
+
+data = room.SerializeToString()
+
+print(len(data))
+print(data)
+
+room2 = room_pb2.Room()
+room2.ParseFromString(data)
+
+print(room2)
